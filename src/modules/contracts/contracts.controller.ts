ca04bc7b8d07a2570f8c542bae948a83a31a29b8
javier15975 <javier15975@hotmail.com>
2025-11-12 17:50:40 -0500

    update contracts.controller.ts

--- src/modules/contracts/contracts.controller.ts
+++ src/modules/contracts/contracts.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpCode, Post, HttpStatus, Get, Param } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger'
 
 import { ContractsService } from './contracts.service'
 import { CreateContractDto } from './dto/create-contract.dto'
@@ -11,13 +11,20 @@ import { CreateContractDto } from './dto/create-contract.dto'
 export class ContractsController {
   constructor(private readonly contractsService: ContractsService) {}
 
-  @HttpCode(HttpStatus.CREATED)
   @Post('/create')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Crear un contrato en Keynua' })
+  @ApiBody({ type: CreateContractDto })
+  @ApiResponse({ status: 201, description: 'Contrato creado correctamente' })
   create(@Body() createContractDto: CreateContractDto) {
     return this.contractsService.create(createContractDto)
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener información de un contrato' })
+  @ApiParam({ name: 'id', description: 'ID del contrato' })
+  @ApiResponse({ status: 200, description: 'Detalle del contrato retornado' })
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.contractsService.findOne(id)
   }
